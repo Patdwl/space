8fba2c71592769a8a7699b7ec1dd4f623b24dbdf
thkruz <theodore.kruczek@gmail.com>
2021-10-01 11:00:42 +0000

    Deploying to gh-pages from  @ 509245fb28b2bc9d86c6eb2d9c268c3a609646e0 🚀

--- analysis/js/analysis-tools.js
+++ analysis/js/analysis-tools.js
@@ -18642,10 +18642,14 @@ var __read = (undefined && undefined.__read) || function (o, n) {
     }
     return ar;
 };
-var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
+var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
 var keepTrackApi = window.keepTrackApi || {
     html: function (strings) {
@@ -18669,7 +18673,7 @@ var keepTrackApi = window.keepTrackApi || {
             }
             finally { if (e_1) throw e_1.error; }
         }
-        return String.raw.apply(String, __spreadArray([strings], __read(placeholders)));
+        return String.raw.apply(String, __spreadArray([strings], __read(placeholders), false));
     },
     register: function (params) {
         // If this is a valid callback
